d32e18b9fb332218cf2b2ffdcf110c4153691e60
haha0ha <haha0hazzang@gmail.com>
2024-10-25 19:27:59 +0900

    List filtter update

--- src/Component/List.jsx
+++ src/Component/List.jsx
@@ -3,6 +3,7 @@ import './List.css'
 import TodoItem from './TodoItem';
 import { TodoStateContext } from '../App';
 import { CalendarCheck } from 'react-bootstrap-icons';
+import classNames from 'classnames';
 
 
 const List = () =>{
@@ -10,17 +11,29 @@ const List = () =>{
     const todos = useContext(TodoStateContext);
 
     const [search, setSearch] = useState('');
+    const [filter, setFilter] = useState('all');
+
 
     const onChangeSearch=(e)=>{
         setSearch(e.target.value);
     }
 
     const getFilteredData = () =>{
-        if(search===''){
-            return todos;
+        let filteredTodos = todos;
+        
+        //완/미완 필터링
+        if(filter === 'done'){
+            filteredTodos = todos.filter((todo)=>todo.isDone);
+        } else if(filter ==='notDone'){
+            filteredTodos = todos.filter((todo)=>!todo.isDone);
         }
-        return todos.filter((todo)=>
-        todo.content.toLowerCase().includes(search.toLowerCase()));
+
+        //검색어 필터링
+        if(search !== ''){
+            filteredTodos = filteredTodos.filter((todo)=>
+                todo.content.toLowerCase().includes(search.toLowerCase()));
+        }
+        return filteredTodos;
     }
 
     const filteredData = getFilteredData();
@@ -37,9 +50,15 @@ const List = () =>{
     return(
         <div className="List">
             <div className='count'>
-                <div className='count-item'>🔥 To do <span>{totalCount}</span> </div>
-                <div className='count-item'>👌 Finish <span>{doneCount}</span> </div>
-                <div className='count-item'>👏 Rest <span>{notDoneCount}</span> </div>
+                <div className= {classNames('count-item all',{active:filter ==='all'})}
+                onClick={()=>setFilter('all')}>
+                    🔥To do <span>{totalCount} </span> </div>
+                <div className= {classNames('count-item done',{active:filter ==='done'})}
+                onClick={()=>setFilter('done')}>
+                    👌Finish <span>{doneCount} </span> </div>
+                <div className={classNames('count-item notDone',{active:filter ==='notDone'})}
+                onClick={()=>setFilter('notDone')}>
+                    👏Rest <span>{notDoneCount} </span> </div>
             </div>
             <input value={search}
             placeholder='Search'
@@ -47,7 +66,7 @@ const List = () =>{
             ></input>
             <div className="todo-wapper">
                 {filteredData.length === 0 ? (
-                    <div className='epmty_message'>
+                    <div className='epmty-message'>
                         <CalendarCheck className="calendar-icon"/>
                         새로운 일정을 추가해 보세요!
                     </div>    
